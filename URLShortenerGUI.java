11ee1c1df965a36c7fc74a182ec20e16fe878c07
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-10-23 18:05:00 +0530

    Refactor URLShortenerGUI class formatting and add fields

--- URLShortenerGUI.java
+++ URLShortenerGUI.java
@@ -4,7 +4,10 @@ import java.awt.event.*;
 import java.net.URI;
 import java.util.*;
 
-public class URLShortenerGUI {
-
+public class URLShortenerGUI{
+    private Map<String, String> shortToUrl = new HashMap<>();
+    private static final String CHAR_POOL = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final int CODE_LENGTH = 6;
+    private Random random = new Random();
     
 }
