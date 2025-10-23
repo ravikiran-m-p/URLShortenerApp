3a864c1136a0026d58dcfe8dd8d3deb6faa295fa
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-10-23 18:06:42 +0530

    Clarify CODE_LENGTH with a descriptive comment
    Added a comment to clarify the purpose of CODE_LENGTH.

--- URLShortenerGUI.java
+++ URLShortenerGUI.java
@@ -4,10 +4,11 @@ import java.awt.event.*;
 import java.net.URI;
 import java.util.*;
 
-public class URLShortenerGUI{
+public class URLShortenerGUI
+{
     private Map<String, String> shortToUrl = new HashMap<>();
     private static final String CHAR_POOL = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final int CODE_LENGTH = 6;
+    private static final int CODE_LENGTH = 6; // you can take ur own length of url value
     private Random random = new Random();
     
 }
