19ca2e5639c1ff510400dd372e96eb0d2f61be5f
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-10-24 18:46:06 +0530

    Initialize JFrame for URL Shortener GUI

--- URLShortenerGUI.java
+++ URLShortenerGUI.java
@@ -24,6 +24,10 @@ private String generateShortCode()
 
 public URLShortenerGUI()
 {
+    JFrame frame = new JFrame("URL Shortener");
+    frame.setSize(500, 200);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
     JTextField urlField = new JTextField(30);
     JButton shortenBtn = new JButton("Shorten");
     JLabel resultLabel = new JLabel("");
